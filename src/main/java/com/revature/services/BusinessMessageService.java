b49ae6f63a1067cad929bf54a181751b35e7ea45
Matthew <matt_alston2112@yahoo.com>
2019-07-10 22:38:48 -0400

    changes to eventmessageservice, businessmessageservice

--- src/main/java/com/revature/services/BusinessMessageService.java
+++ src/main/java/com/revature/services/BusinessMessageService.java
@@ -3,9 +3,9 @@ package com.revature.services;
 import com.revature.pojos.BusinessMessage;
 
 public interface BusinessMessageService {
-	public void saveMessage(BusinessMessage bmess, int businessID);
-	public void sendMessage(String message, int Eid, int Bid);
-	public BusinessMessage getBusinessMessageByBusiness(int Bid);
+	public void saveMessage(BusinessMessage bMessage, int businessId);
+	public void sendMessage(String message, int EId, int BId);
+	public BusinessMessage getBusinessMessageByBusiness(int BId);
 	
 
 }
