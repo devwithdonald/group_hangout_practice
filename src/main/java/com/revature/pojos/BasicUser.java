89fc14d99e3856778b3a938d69f196ebd3a11431
devwithdonald <hend.donald@gmail.com>
2019-07-10 21:07:09 -0400

    added BasicUser Pojo

---
+++ src/main/java/com/revature/pojos/BasicUser.java
@@ -0,0 +1,90 @@
+package com.revature.pojos;
+
+import java.util.List;
+
+public class BasicUser extends User{
+	String firstName;
+	String lastName;
+	List<Friends> friendList;
+	List<Subscriptions> subscriptionList;
+	
+	public BasicUser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public BasicUser(int id, String username, String password) {
+		super(id, username, password);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BasicUser(String firstName, String lastName, List<Friends> friendList,
+			List<Subscriptions> subscriptionList) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.friendList = friendList;
+		this.subscriptionList = subscriptionList;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public List<Friends> getFriendList() {
+		return friendList;
+	}
+	public void setFriendList(List<Friends> friendList) {
+		this.friendList = friendList;
+	}
+	public List<Subscriptions> getSubscriptionList() {
+		return subscriptionList;
+	}
+	public void setSubscriptionList(List<Subscriptions> subscriptionList) {
+		this.subscriptionList = subscriptionList;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BasicUser other = (BasicUser) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "BasicUser [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+	
+	
+}
