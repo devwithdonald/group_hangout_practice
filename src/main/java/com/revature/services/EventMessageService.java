d133502f0d81ccaa5ba4c8eb7a720a34364114dc
devwithdonald <hend.donald@gmail.com>
2019-07-11 21:04:08 -0400

    finished basicUserService interface

--- src/main/java/com/revature/services/EventMessageService.java
+++ src/main/java/com/revature/services/EventMessageService.java
@@ -3,6 +3,6 @@ package com.revature.services;
 import com.revature.pojos.EventMessage;
 
 public interface EventMessageService {
-	public void saveEMessage(EventMessage eventMessage, int SenderId, String timeStamp);
-	public void sendEMessage(EventMessage eventMessage, String senderName);
+	public void saveEventMessage(EventMessage eventMessage, int SenderId, String timeStamp);
+	public void sendEventMessage(EventMessage eventMessage, String senderName);
 }
