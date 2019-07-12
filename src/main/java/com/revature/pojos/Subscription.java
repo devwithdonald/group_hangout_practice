61dd576d5c929b8b358d40197fd3a5d3a4bb4495
glozy <glozy94@gmail.com>
2019-07-12 18:58:45 -0400

    fixed naming convention in Subscription Pojo

--- src/main/java/com/revature/pojos/Subscription.java
+++ src/main/java/com/revature/pojos/Subscription.java
@@ -18,15 +18,15 @@ public class Subscription {
 	@Id
 	@Column(name="SUBSCRIPTION_ID")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int subscriptionID;
+	private int subscriptionId;
 	
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinColumn(name="USER_ID")
-	private int userID;
+	private int userId;
 	
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinColumn(name="BUSINESS_ID")
-	private int businessID;
+	private int businessId;
 	
 	@Column(name="TIME_OF_SUB")
 	private String timeOfSub;
@@ -34,10 +34,10 @@ public class Subscription {
 	@Column(name="RSVP_COUNT")
 	private int rsvpCount;
 	
-	public Subscription(int userID, int businessID, String timeOfSub, int rsvpCount) {
+	public Subscription(int userId, int businessId, String timeOfSub, int rsvpCount) {
 		super();
-		this.userID = userID;
-		this.businessID = businessID;
+		this.userId = userId;
+		this.businessId = businessId;
 		this.timeOfSub = timeOfSub;
 		this.rsvpCount = rsvpCount;
 	}
@@ -48,7 +48,7 @@ public class Subscription {
 	
 	@Override
 	public String toString() {
-		return "Subscription [subscriptionID=" + subscriptionID + ", userID=" + userID + ", businessID=" + businessID
+		return "Subscription [subscriptionId=" + subscriptionId + ", userId=" + userId + ", businessId=" + businessId
 				+ ", timeOfSub=" + timeOfSub + ", rsvpCount=" + rsvpCount + "]";
 	}
 	
@@ -56,11 +56,11 @@ public class Subscription {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + businessID;
+		result = prime * result + businessId;
 		result = prime * result + rsvpCount;
-		result = prime * result + subscriptionID;
+		result = prime * result + subscriptionId;
 		result = prime * result + ((timeOfSub == null) ? 0 : timeOfSub.hashCode());
-		result = prime * result + userID;
+		result = prime * result + userId;
 		return result;
 	}
 	
@@ -73,44 +73,44 @@ public class Subscription {
 		if (getClass() != obj.getClass())
 			return false;
 		Subscription other = (Subscription) obj;
-		if (businessID != other.businessID)
+		if (businessId != other.businessId)
 			return false;
 		if (rsvpCount != other.rsvpCount)
 			return false;
-		if (subscriptionID != other.subscriptionID)
+		if (subscriptionId != other.subscriptionId)
 			return false;
 		if (timeOfSub == null) {
 			if (other.timeOfSub != null)
 				return false;
 		} else if (!timeOfSub.equals(other.timeOfSub))
 			return false;
-		if (userID != other.userID)
+		if (userId != other.userId)
 			return false;
 		return true;
 	}
 	
-	public int getSubscriptionID() {
-		return subscriptionID;
+	public int getSubscriptionId() {
+		return subscriptionId;
 	}
 	
-	public void setSubscriptionID(int subscriptionID) {
-		this.subscriptionID = subscriptionID;
+	public void setSubscriptionId(int subscriptionID) {
+		this.subscriptionId = subscriptionID;
 	}
 	
-	public int getUserID() {
-		return userID;
+	public int getUserId() {
+		return userId;
 	}
 	
-	public void setUserID(int userID) {
-		this.userID = userID;
+	public void setUserId(int userID) {
+		this.userId = userID;
 	}
 	
-	public int getBusinessID() {
-		return businessID;
+	public int getBusinessId() {
+		return businessId;
 	}
 	
-	public void setBusinessID(int businessID) {
-		this.businessID = businessID;
+	public void setBusinessId(int businessID) {
+		this.businessId = businessID;
 	}
 	
 	public String getTimeOfSub() {
